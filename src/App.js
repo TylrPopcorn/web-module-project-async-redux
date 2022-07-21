e464f7f9e7564e8f9255c33077cc7d42064bad09
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-07-20 23:57:58 -0400

    Finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+import styled from "styled-components"
+
+import { ASYNC } from "./actions/Async"
+
+function App(props) {
+  const { Assets } = props;
+  console.log("APP PROPS: ", props)
+
+  useEffect(() => {
+    props.Async() //Run an axios API call.
+  }, [])
+
+  const handleClick = (e) => {
+    //  e.preventDefault()
+    // props.Async()
+  }
+
+  const makeLabel = () => {
+    //DISPLAY API DATA IN A COMPONENT
+
+    return (
+      <div className="List">
+        {
+          Assets.map(val => {
+            // console.log(val)
+            return (
+              <div className="Card" key={val.assetID}>
+                <p className="ItemName">{val.name}</p>
+                <p className="ItemId">{val.id} </p>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      Async Redux Project
+      <h2 className="title">Assets</h2>
+
+      <form onSubmit={handleClick}>
+        <label>
+          <button >REFRESH</button>
+        </label>
+      </form>
+
+      {
+        makeLabel()
+      }
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  //console.log(state)
+  return state
+}
+
+export default connect(mapStateToProps, { Async: ASYNC })(App);
